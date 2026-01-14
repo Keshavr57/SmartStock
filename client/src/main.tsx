7711fb9ffbe5b82b543f0b4c493b9b2005c451f9
Keshav Rajput <keshavraj9954@gmail.com>
2026-01-14 21:19:11 +0530

    FORCE REBUILD: Delete cache, update version to 2.0.2, add build timestamp

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client"
 import App from "./App"
 import "./index.css"
 
+// Force cache invalidation - Build: 2026-01-14-v3
+console.log('🚀 SmartStock v2.0.1 - Build: 2026-01-14T13:00:00Z');
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
